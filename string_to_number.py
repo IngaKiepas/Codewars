85d1f4a6b5071c59afa86c0b22aedd90bc00fdbf
inga <kiepas.inga@gmail.com>
2024-10-09 11:46:48 +0200

    Function that transform a string into a number

---
+++ string_to_number.py
@@ -0,0 +1,11 @@
+# We need a function that can transform a string into a number.
+# All inputs will be strings and every string is a representation of an integral number
+# Examples:
+# "1234" -> 1234
+# "605" -> 605
+# "1405" -> 1405
+# "-7" -> -7
+
+def string_to_number(s):
+    return int(s)
+
