0162e1a3a1d40d8b649f81ff394f94c1107b29b4
inga <kiepas.inga@gmail.com>
2024-10-09 11:52:49 +0200

    Function that return a repeated string

---
+++ Python solutions/repeat_string.py
@@ -0,0 +1,8 @@
+# Write a function that accepts an integer n and a string s as parameters and return a string
+# of s repeated exactly n times
+# Examples:
+# 6, "I" -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_string(repeat, string):
+    return string * repeat
